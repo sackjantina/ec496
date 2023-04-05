91c3f7c03167464fb1422b763702987e771a8308
Jack Santina <m235580@usna.edu>
2023-04-05 10:58:58 -0400

    updates

--- vio/visual/calibration.py
+++ vio/visual/calibration.py
@@ -2,4 +2,11 @@ import cv2
 import numpy as np
 import glob
 
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + CV2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+chessboardSize = (7, 7)
+frameSize = (640, 480)
+
+objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 
